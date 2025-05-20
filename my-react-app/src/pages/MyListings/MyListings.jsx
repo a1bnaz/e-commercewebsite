609a5d824a8a947a14c97acb87383dd4b2d44a13
albert <ieatbaconhair@gmail.com>
2025-05-20 18:45:36 -0400

    designed the my listings page, looks ugly but wtv

--- my-react-app/src/pages/MyListings/MyListings.jsx
+++ my-react-app/src/pages/MyListings/MyListings.jsx
@@ -3,14 +3,14 @@
 import Navbar from "../../components/Navbar/NavBar";
 
 import styles from "./MyListings.module.css";
-
+import sideeyedog from "../../assets/sideeyedog.png";
 
 
 export default function UserListings() {
     
     return (
         <>
-            <Navbar />
+            
             {/* display only what belongs to the user... send a request to the api to check what products belong to the user that is in the current localstorage session */}
 
             {/* 
@@ -20,8 +20,34 @@ export default function UserListings() {
             
             
             */}
-            <div>
-                User Listings
+            <div className={styles.container}>
+                <Navbar />
+
+                <div className={styles.heading}>
+                    <p className={styles.header}>My Listings</p>
+
+                    <button className={styles.createListingButton}>Create Listing</button>
+                </div>
+
+                <div className={styles.listingsGrid}>
+                    <div className={styles.listing}>
+
+                        <div className={styles.listingImageContainer}>
+                            <img className={styles.listingImage} src={sideeyedog} />
+                        </div>
+
+                        <div className={styles.listingInformationContainer}>
+                            <p className={styles.listingName}>name</p>
+                            <p className={styles.listingPrice}>price</p>
+                        </div>
+
+                        <div className={styles.listingButtonsContainer}>
+                            <button>Edit Listing</button>
+                            <button>Delete Listing</button>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </>
     )
