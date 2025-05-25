6a55226b41977af953b137dfaf7f6ffd5a915896
albert <ieatbaconhair@gmail.com>
2025-05-25 16:05:50 -0400

    finished profile modal

--- my-react-app/src/components/ProfileModal/ProfileModal.jsx
+++ my-react-app/src/components/ProfileModal/ProfileModal.jsx
@@ -1,9 +1,11 @@
 
+import { useNavigate } from "react-router-dom";
 import { useRef, useEffect } from "react";
 
 import styles from "./ProfileModal.module.css";
 
-export default function ProfileModal({onClose}) {
+export default function ProfileModal({ onClose }) {
+    const navigate = useNavigate();
     const modalRef = useRef(null);
 
     useEffect(() => {
@@ -20,11 +22,24 @@ export default function ProfileModal({onClose}) {
         };
     }, [onClose]);
 
+    function handleProfileButton() {
+        console.log("profile button clicked")
+    }
+
+    function handleSettingsButton() {
+        console.log("settings button clicked")
+    }
+
+    function handleLogoutButton() {
+        navigate("/login");
+        onClose();
+    }
+
     return (
         <div ref={modalRef} className={styles.modalContainer}>
-            <button className={styles.modalButton}>Profile</button>
-            <button className={styles.modalButton}>Settings</button>
-            <button className={styles.modalButton} onClick={onClose}>Logout</button>
+            <button onClick={handleProfileButton} className={styles.modalButton}>Profile</button>
+            <button onClick={handleSettingsButton} className={styles.modalButton}>Settings</button>
+            <button onClick={handleLogoutButton}  className={styles.modalButton}>Logout</button>
         </div>
     )
 }
