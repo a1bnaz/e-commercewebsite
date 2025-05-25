397d66aedfbd41bd52cd1e80fe5554c2b93afa1a
albert <ieatbaconhair@gmail.com>
2025-05-25 17:26:32 -0400

    added react query, updated notes, implemented react query into ViewListing page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home.jsx";
@@ -9,35 +10,37 @@ import MyListings from "./pages/MyListings/MyListings.jsx";
 import ViewListing from "./pages/ViewListing/ViewListing.jsx";
 
 function App() {
-  
+  const queryClient = new QueryClient();
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          
-          {/* Landing Page/First Page */}
-          <Route path="/" element={<Login />} />
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            
+            {/* Landing Page/First Page */}
+            <Route path="/" element={<Login />} />
 
-          {/* Login Page */}
-          <Route path="/login" element={<Login />} />
+            {/* Login Page */}
+            <Route path="/login" element={<Login />} />
 
-          {/* Home Page */}
-          <Route path="/home" element={<Home />} />
+            {/* Home Page */}
+            <Route path="/home" element={<Home />} />
 
-          {/* Listings Page */}
-          <Route path="/listings" element={<Listings />} />
+            {/* Listings Page */}
+            <Route path="/listings" element={<Listings />} />
 
-          <Route path="/listings/:listingId" element={<ViewListing/>} />
+            <Route path="/listings/:listingId" element={<ViewListing/>} />
 
-          {/* My Listings Page */}
-          <Route path="/mylistings" element={<MyListings />} />
-          
-          {/* Error Page */}
-          <Route path="*" element={<Error />} /> 
+            {/* My Listings Page */}
+            <Route path="/mylistings" element={<MyListings />} />
+            
+            {/* Error Page */}
+            <Route path="*" element={<Error />} /> 
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
   )
 }
--- my-react-app/src/pages/ViewListing/ViewListing.jsx
+++ my-react-app/src/pages/ViewListing/ViewListing.jsx
@@ -1,4 +1,83 @@
 
+import { useQuery } from "@tanstack/react-query";
+import { Link, useParams } from "react-router-dom"; 
+    
+import styles from "./ViewListing.module.css";
+import Navbar from "../../components/Navbar/NavBar";
+
+import sideeyedog from "../../assets/sideeyedog.png";
+
+
+async function getListingInformation(listingId) {
+    const response = await fetch(`http://localhost:8080/api/products/${listingId}`);
+    if (!response.ok) {
+        throw new Error("Failed to fetch listing!");
+    }
+    return response.json();
+}
+
+export default function ViewListing() {
+    const { listingId } = useParams();
+
+    const {data: listing, isLoading, error} = useQuery({
+        queryKey: ["listingQuery", listingId], // unique identifier for the query
+        queryFn: () => getListingInformation(listingId), // the function that query uses to fetch data
+        enabled: !!listingId, // only run this query if listingId exists and is not empty, null, or undefined. the first ! converts the value to a boolean and inverts it. the second ! inverts it back, so you get true for any "truthy" value and false for any "falsy" value.
+        /*
+        examples:
+        if listingId is "123", !!listingId is true.
+        if listingId is null, !!listingId is false.
+        if listingId is undefined, !!listingId is false.
+        if listingId is "", !!listingId is false.
+        */
+    });
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+    if (error) {
+        return <div>Error loading listing.</div> 
+    }
+    if (!listing) {
+        return <p>Data not loaded...</p>
+    }
+
+    function handleAddToCartButton() {
+        console.log("added to cart")
+    }
+
+    return (
+        <div className={styles.listingContainer}>
+            <Navbar />
+            
+            <div className={styles.listingContent}>
+                <div className={styles.listingImageContainer}>
+                    <img className={styles.listingImage} src={sideeyedog} />
+                </div>
+
+                <h2 className={styles.listingName}>{listing.name}</h2>
+
+                <hr className={styles.listingDivider} />
+                <Link className={styles.listingOwnerUsernameLink}><p className={styles.listingOwnerUsername}>{listing.user.username}</p></Link>
+
+                <p className={styles.listingPrice}>Price: ${listing.price}</p>
+
+                <button onClick={handleAddToCartButton} className={styles.listingAddToCartButton}>Add to Cart</button>
+
+                <h2 className={styles.listingDescriptionHeader}>Description</h2>
+
+                <div className={styles.listingDescriptionContainer}>
+                    <p className={styles.listingDescription}>{listing.description}</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+/*
+
+    
 import { Link, useParams } from "react-router-dom"; 
     
 import styles from "./ViewListing.module.css";
@@ -64,3 +143,5 @@ export default function ViewListing() {
         </div>
     )
 }
+
+*/
